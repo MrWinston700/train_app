6954d95c8c746c2df4e491ce75aae32094d1ed50
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-02-28 22:16:11 -0500

    added params to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,28 @@ class UsersController < ApplicationController
 
     def create
         if @user 
-            @user.create(user_params) 
+            @user = User.create(user_params) 
+
         else 
             render new_user_path
         end
     end 
 
     def edit 
-        @user.find(params[:id])
+        @user = User.find(params[:id])
     end 
 
     def update
-        
+
     end 
+
+    def show 
+        @user = User.find(params[:id])
+    end
 
     private 
 
     def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 end
