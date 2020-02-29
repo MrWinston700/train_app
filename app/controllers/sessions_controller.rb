434e64af2042d178975f5a9744b3668cbfd554c4
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-02-29 16:10:03 -0500

    added sessions controller method as well as a view to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+    def home     
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+  
+      @user = User.find_by(name: params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        log_in @user 
+        redirect_to user_path(@user)
+      else
+        flash[:error] = 'Invalid name and/or password combination'
+        redirect_to signin_path
+      end
+    end
+  
+  
+    def destroy
+      log_out
+      redirect_to root_path '/'
+    end
+end
+
 end
