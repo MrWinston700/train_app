49d9ad75d42903be244efff628ffbfe639a6d9db
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-02-28 21:25:07 -0500

    created methods for Users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end 
+
+    def edit 
+
+    end 
+
+    def update
+    end 
+
+    private 
+
+    def user_params
+    end
 end
