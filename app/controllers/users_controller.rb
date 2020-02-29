ea2cea7548207e106801293512a632c3fa145f3d
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-02-28 21:54:40 -0500

    added functionalities to users controller and create sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        if @user 
+            @user.create(user_params) 
+        else 
+            render new_user_path
+        end
     end 
 
     def edit 
-
+        @user.find(params[:id])
     end 
 
     def update
+        
     end 
 
     private 
