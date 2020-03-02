07454f3c551cdfd1c7198bda7025c77b5ea635a6
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-03-02 15:23:54 -0500

    slight name change in sessions*new view and some refactoring in the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,9 @@ class SessionsController < ApplicationController
     def create
       if request.env['omniauth.auth'] != nil
         @user = User.find_by(name: request.env['omniauth.auth']['info']['name'])
-
+          
         @user = User.create(name: request.env['omniauth.auth']['info']['name'], password: SecureRandom.hex) unless @user !=nil
-
+        binding.pry
         log_in @user 
         redirect_to user_path(@user)
       else
