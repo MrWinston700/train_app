a567767fb095c6d537cbbf2039f24acf356b7dd5
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-03-03 19:54:02 -0500

    added functionalities to the nav bar in application.html.erb file

--- app/controllers/train_routes_controller.rb
+++ app/controllers/train_routes_controller.rb
@@ -2,5 +2,7 @@ class TrainRoutesController < ApplicationController
     def create
         @train_route = TrainRoute.find_by(id: params[:route_id])
         current_user.train_routes << @train_route 
+
+        redirect_to user_path(current_user)
     end
 end
